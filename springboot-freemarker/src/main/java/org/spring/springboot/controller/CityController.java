4824ebd9fdc810925a3fdfb72a466ea61551cc49
zhengyunfei <zhengyf@ctyun.cn>
2017-04-19 16:47:52 +0800

    springboot集成freemarker

---
+++ springboot-freemarker/src/main/java/org/spring/springboot/controller/CityController.java
@@ -0,0 +1,37 @@
+package org.spring.springboot.controller;
+
+import org.spring.springboot.domain.City;
+import org.spring.springboot.service.CityService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+/**
+ * 城市 Controller 实现 Restful HTTP 服务
+ * <p>
+ * Created by 贤名 on 07/02/2017.
+ * qq 799078779
+ * qq群 541122375
+ */
+@Controller
+public class CityController {
+
+    @Autowired
+    private CityService cityService;
+
+    @RequestMapping(value = "/api/city/{id}", method = RequestMethod.GET)
+    public String findOneCity(Model model, @PathVariable("id") Long id) {
+        model.addAttribute("city", cityService.findCityById(id));
+        return "city";
+    }
+
+    @RequestMapping(value = "/api/city", method = RequestMethod.GET)
+    public String findAllCity(Model model) {
+        List<City> cityList = cityService.findAllCity();
+        model.addAttribute("cityList",cityList);
+        return "cityList";
+    }
+}
